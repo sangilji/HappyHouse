1e026dd633db431bbc242ecc89f12786e3e4bad6
Yudaeun <udu5771@naver.com>
2023-05-16 13:37:53 +0900

    feat: vue FrontEnd

---
+++ HomeFE/homevue/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+	<div id="app">			
+		<!-- {{ this.$store.state.country }} -->
+    <nav>
+			<router-link to="/input">추가</router-link> &nbsp;
+			<router-link to="/list">목록</router-link>			
+		</nav>
+		<router-view key="$route.path"></router-view>
+    <!-- key="$route.path": 같은 경로일때 로드가 되지 않는 문제..절대 경로로 확인되는 경로를 다시 연결하여 노출시키는 방법이다. -->
+	</div>
+</template>
+
+<script>
+	export default {
+    
+	};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ HomeFE/homevue/src/views/HomeMainView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div  class="about">
+    <house-header></house-header>
+    <!-- <house-input></house-input> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import TodoInput from '../components/TodoInput.vue'
+import HouseHeader from '@/components/HouseHeader.vue';
+export default {
+  name: 'HouseMainView',
+  components: {
+    // TodoInput,
+    HouseHeader
+  }
+}
+</script>
+
+    <!-- TodoFooter -->
